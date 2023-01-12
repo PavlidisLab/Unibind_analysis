b2590eb2d4ed1122b83b55136d299f30445dba40
Alexander-Morin <alexm.14@outlook.com>
2023-01-12 11:58:05 -0800

    init config file

---
+++ R/00_config.R
@@ -0,0 +1,13 @@
+## Establishing pathing
+## -----------------------------------------------------------------------------
+
+
+dat_dir <- "/space/grp/amorin/"
+
+
+# Path of Unibind data download
+
+rob_path_mm <- "/space/grp/amorin/Peak_files/Unibind/Mm10/Robust/mm10_TFBS_per_TF.tar.gz"
+rob_path_hg <- "/space/grp/amorin/Peak_files/Unibind/Hg38/Robust/hg38_TFBS_per_TF.tar.gz"
+perm_path_mm <- "/space/grp/amorin/Peak_files/Unibind/Mm10/Permissive/mm10_TFBS_per_TF.tar.gz"
+perm_path_mm <- "/space/grp/amorin/Peak_files/Unibind/Hg38/Permissive/hg38_TFBS_per_TF.tar.gz"
