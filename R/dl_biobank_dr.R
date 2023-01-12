5ebb9f7ca3dc5e38ff287f8e98193ccac396e792
Alexander-Morin <alexm.14@outlook.com>
2023-01-12 11:59:12 -0800

    folded into 02_download

--- R/dl_biobank_dr.R
+++
@@ -1,7 +0,0 @@
-outfile <- "~/scratch/UK_biobank_depletion_ranks.gz"
-url <- "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC9329122/bin/41586_2022_4965_MOESM3_ESM.gz"
-
-if (!file.exists(outfile)) {
-  options(timeout = 1000)  # default 60 was insufficient
-  download.file(url, outfile)  
-}
