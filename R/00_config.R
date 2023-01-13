cce98d135d14019941c793eda940ce7a81949d37
Alexander-Morin <alexm.14@outlook.com>
2023-01-12 18:14:22 -0800

    uk biobank path

--- R/00_config.R
+++ R/00_config.R
@@ -5,9 +5,12 @@
 dat_dir <- "/space/grp/amorin/"
 
 
-# Path of Unibind data download
+# Path of compressed Unibind download
+rob_path_hg <- "/space/grp/amorin/Peak_files/Unibind/Hg38/Robust/damo_hg38_TFBS_per_TF.tar.gz"
+rob_path_mm <- "/space/grp/amorin/Peak_files/Unibind/Mm10/Robust/damo_mm10_TFBS_per_TF.tar.gz"
+perm_path_hg <- "/space/grp/amorin/Peak_files/Unibind/Hg38/Permissive/damo_hg38_TFBS_per_TF.tar.gz"
+perm_path_mm <- "/space/grp/amorin/Peak_files/Unibind/Mm10/Permissive/damo_mm10_TFBS_per_TF.tar.gz"
 
-rob_path_mm <- "/space/grp/amorin/Peak_files/Unibind/Mm10/Robust/mm10_TFBS_per_TF.tar.gz"
-rob_path_hg <- "/space/grp/amorin/Peak_files/Unibind/Hg38/Robust/hg38_TFBS_per_TF.tar.gz"
-perm_path_mm <- "/space/grp/amorin/Peak_files/Unibind/Mm10/Permissive/mm10_TFBS_per_TF.tar.gz"
-perm_path_mm <- "/space/grp/amorin/Peak_files/Unibind/Hg38/Permissive/hg38_TFBS_per_TF.tar.gz"
+
+# Path of UK biobank depletion ranks
+dr_path <- "/space/scratch/amorin/UK_biobank_depletion_ranks.gz"
