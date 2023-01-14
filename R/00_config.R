67e0d6f2e5e8fc2d211b72c883410b694f1e1750
Alexander-Morin <alexm.14@outlook.com>
2023-01-14 12:39:47 -0800

    some global vars

--- R/00_config.R
+++ R/00_config.R
@@ -4,6 +4,17 @@
 
 dat_dir <- "/space/grp/amorin/"
 
+# For use in parallel
+cores <- 8
+
+# Minimum peaks to be considered for analysis
+min_peaks <- 100
+
+
+# Unibind metadata
+# TODO: reconsider pathing
+meta_outfile <- "~/scratch/R_objects/Unibind_metadata.RDS"
+
 
 # Path of compressed Unibind download
 rob_path_hg <- paste0(dat_dir, "Peak_files/Unibind/Hg38/Robust/damo_hg38_TFBS_per_TF")
@@ -13,11 +24,15 @@ perm_path_mm <- paste0(dat_dir, "Peak_files/Unibind/Mm10/Permissive/damo_mm10_TF
 
 
 # TODO: formalize package or download functions
-ref_hg <- paste(dat_dir, "Metadata/refseq_select_hg38.tsv")
+ref_hg <- paste0(dat_dir, "Metadata/refseq_select_hg38.tsv")
 ref_mm <- paste0(dat_dir, "Metadata/refseq_select_mm10.tsv")
+bl_path_hg <-  paste0(dat_dir, "Chromosome_info/blacklist_hg38.tsv")
+bl_path_mm <- paste0(dat_dir, "Chromosome_info/blacklist_hg38.tsv")
 
-bl_hg <-  paste0(dat_dir, "Chromosome_info/blacklist_hg38.tsv")
-bl_mm <- paste0(dat_dir, "Chromosome_info/blacklist_hg38.tsv")
+
+# Raw bindscore matrices
+bmat_path_hg <- "/space/scratch/amorin/R_objects/unibind_all_scores_hg.RDS"
+bmat_path_mm <- "/space/scratch/amorin/R_objects/unibind_all_scores_mm.RDS"
 
 
 # Path of UK biobank depletion ranks
