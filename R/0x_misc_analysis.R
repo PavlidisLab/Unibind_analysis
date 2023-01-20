cae4c66df8a081317fa77ecf0165888eee3c0b26
Alexander-Morin <alexm.14@outlook.com>
2023-01-19 21:07:43 -0800

    collapsing prelim/scratch analysis from first draft into misc

--- R/0x_misc_analysis.R
+++ R/0x_misc_analysis.R
@@ -1,9 +1,13 @@
-## Analysis of Unibind bind matrices
-## TODO: organize functions
-# TODO: explicit clustering here?
-# TODO: describe range
-# TODO: better description of top cor pairs across species
+## This script is a placeholder for work that was started but is incomplete, or
+## for quick hits that were written for an earlier draft of this project. It
+## therefore contains (messy) code that currently will not run. 
 ## -----------------------------------------------------------------------------
+
+
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# The following was written to begin exploring the Unibind bind score matrices,
+# getting summaries of binding and exploring the similarity of experiments.
+# ------------------------------------------------------------------------------
 
 library(tidyverse)
 library(reshape2)
@@ -478,7 +482,7 @@ p1 <-
   theme_classic() +
   theme(axis.title = element_text(size = 20),
         axis.text = element_text(size = 20))
-  
+
 
 
 
@@ -636,3 +640,388 @@ ggplot(udf_mm, aes(x = X1, y = X2)) +
 
 # PC1 vs UMAP 1
 plot(umap_l$Mouse$layout[, 1], pc_l$Mouse$PC$x[, 1])
+
+
+
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# The following was written to compare results obtained using the permissive
+# versus robust collection of Unibind data. It is outdated and will not run.
+# ------------------------------------------------------------------------------
+
+
+library(tidyverse)
+library(reshape2)
+library(parallel)
+library(pheatmap)
+library(RColorBrewer)
+source("~/regnetR/R/utils/bscore_unibind_functions.R")
+
+
+# tfs_hg <- read.delim("~/Data/Metadata/human_tfs_lambert2018.tsv", stringsAsFactors = FALSE)
+pc_hg <- read.delim("~/Data/Metadata/ensembl_human_protein_coding_105.tsv", stringsAsFactors = FALSE)
+
+#
+prot <- read.delim("~/Data/Expression_files/HPA/normal_tissue.tsv", stringsAsFactors = FALSE)
+
+#
+allrank <- readRDS("~/scratch/R_objects/Apr2022_ranked_target_list.RDS")
+
+#
+bind_hg <- readRDS("~/scratch/R_objects/unibind_bindscore_robust_human.RDS")
+bind_perm_hg <- readRDS("~/scratch/R_objects/unibind_bindscore_human.RDS")
+bind_mm <- readRDS("~/scratch/R_objects/unibind_bindscore_robust_mouse.RDS")
+bind_perm_mm <- readRDS("~/scratch/R_objects/unibind_bindscore_mouse.RDS")
+
+#
+lt <- read.delim("~/Data/Metadata/Curated_targets_all_Oct2022.tsv")
+
+# lt <- mutate(lt,
+#              TF_Symbol = str_to_upper(TF_Symbol),
+#              Target_Symbol = str_to_upper(Target_Symbol))
+
+
+
+# TFs in permissive but not robust
+diff_hg <- setdiff(names(bind_perm_hg$Group_mean), names(bind_hg$Group_mean))
+diff_mm <- setdiff(names(bind_perm_mm$Group_mean), names(bind_mm$Group_mean))
+
+
+
+# First looking at the concordance of the summarized results from the unibind
+# permissive and robust (required presence of canonical motif) collection
+# ------------------------------------------------------------------------------
+
+
+# Compare mean score from robust and permissive
+# Human PAX6 example of weaker cor (0.6658) between robust and permissive
+
+
+bindscore_cor <- function(robust_l, permissive_l) {
+  
+  cor_l <- lapply(names(robust_l$Group_mean), function(x) {
+    rob <- dplyr::rename(robust_l$Group_mean[[x]], Mean_robust = Mean)
+    perm <- dplyr::rename(permissive_l$Group_mean[[x]], Mean_permissive = Mean)
+    df <- left_join(rob, perm, by = "Symbol")
+    cor(df$Mean_robust, df$Mean_permissive)
+  })
+  
+  names(cor_l) <- names(robust_l$Group_mean)
+  sort(unlist(cor_l))
+}
+
+
+uni_cor_hg <- bindscore_cor(bind_hg, bind_perm_hg)
+uni_cor_mm <- bindscore_cor(bind_mm, bind_perm_mm)
+
+
+# Count of topn genes in mean bind score between the two collections
+
+
+topn <- 500
+
+
+bindscore_topn <- function(robust_l, permissive_l) {
+  
+  topn_l <- lapply(names(robust_l$Group_mean), function(x) {
+    rob <- arrange(robust_l$Group_mean[[x]], desc(Mean))$Symbol[1:500]
+    perm <- arrange(permissive_l$Group_mean[[x]], desc(Mean))$Symbol[1:500]
+    length(intersect(rob, perm))
+  })
+  
+  names(topn_l) <- names(robust_l$Group_mean)
+  sort(unlist(topn_l))
+}
+
+
+uni_topn_hg <- bindscore_topn(bind_hg, bind_perm_hg)
+uni_topn_mm <- bindscore_topn(bind_mm, bind_perm_mm)
+
+
+# Jaccard of diffbind genes for robust and permissive unibind
+
+
+jacc_diffbind <- function(robust_l, permissive_l) {
+  
+  jacc_l <- lapply(names(robust_l$Group_mean), function(x) {
+    rob <- rownames(filter(robust_l$Fit[[x]], logFC > 0 & adj.P.Val < 0.05))
+    perm <- rownames(filter(permissive_l$Fit[[x]], logFC > 0 & adj.P.Val < 0.05))
+    length(intersect(rob, perm)) / length(union(rob, perm))
+  })
+  
+  names(jacc_l) <- names(robust_l$Group_mean)
+  sort(unlist(jacc_l))
+}
+
+
+uni_jacc_hg <- jacc_diffbind(bind_hg, bind_perm_hg)
+uni_jacc_mm <- jacc_diffbind(bind_mm, bind_perm_mm)
+
+
+# Plots
+# ------------------------------------------------------------------------------
+
+
+# Barchart of count of diff bound genes
+
+
+plot_barchart <- function(df) {
+  
+  ggplot(df, aes(x =  reorder(Symbol, N), y = N)) +
+    geom_bar(stat = "identity") +
+    ylab("Count of differentially bound genes") +
+    theme_classic() +
+    theme(axis.title.x = element_blank(),
+          axis.title.y = element_text(size = 25),
+          axis.text.y = element_text(size = 20),
+          axis.text.x = element_text(size = 8, angle = 90, vjust = 0.5, hjust=1),
+          axis.ticks.x = element_blank())
+  
+}
+
+
+plot_l <- lapply(ndiff_l, plot_barchart)
+
+
+# Count of diff bound genes when using robust vs permissive
+
+plot(count_hg$N.x, count_hg$N.y)
+plot(count_mm$N.x, count_mm$N.y)
+
+
+
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# The following was written to compare the aggregated rankings from the ENCODE
+# pipeline to Unibind. This is outdated and will not run.
+# ------------------------------------------------------------------------------
+
+library(tidyverse)
+library(pheatmap)
+
+# The final high-throughput TR rankings
+allrank <- readRDS("~/scratch/R_objects/Apr2022_ranked_target_list.RDS")
+
+# list objects contain the data matrices [robust/permissive]
+bind_hg <- readRDS("~/scratch/R_objects/unibind_bindscore_robust_human.RDS") 
+bind_mm <- readRDS("~/scratch/R_objects/unibind_bindscore_robust_mouse.RDS")
+bind_perm_hg <- readRDS("~/scratch/R_objects/unibind_bindscore_human.RDS")
+bind_perm_mm <- readRDS("~/scratch/R_objects/unibind_bindscore_mouse.RDS")
+
+
+topn <- 500
+
+# Merging allrank with unibind
+
+tf_rank_hg <- intersect(names(allrank$Human), names(bind_perm_hg$Group_mean))
+tf_rank_mm <- intersect(str_to_upper(names(allrank$Mouse)), names(bind_perm_mm$Group_mean))
+
+
+keep_cols <- c(
+  "Symbol",
+  "Count_DE",
+  "Avg_abs_FC",
+  "Mean_bind",
+  "Bind_logFC",
+  "Bind_adj_Pval",
+  "Curated_target",
+  "Rank_perturbation",
+  "Rank_binding",
+  "Rank_integrated"
+)
+
+
+
+allrank_hg <- lapply(tf_rank_hg, function(x) {
+  
+  perm_df <- bind_perm_hg$Fit[[x]][, c("logFC", "adj.P.Val")] %>% 
+    rownames_to_column(var = "Symbol") %>% 
+    left_join(bind_perm_hg$Group_mean[[x]], by = "Symbol")
+  colnames(perm_df) <- c("Symbol", "logFC_permissive", "FDR_permissive", "Mean_permissive")
+  
+  if (!is.null(bind_hg$Fit[[x]])) {
+    
+    rob_df <- bind_hg$Fit[[x]][, c("logFC", "adj.P.Val")] %>% 
+      rownames_to_column(var = "Symbol") %>% 
+      left_join(bind_hg$Group_mean[[x]], by = "Symbol")
+    colnames(rob_df) <- c("Symbol", "logFC_robust", "FDR_robust", "Mean_robust")
+    
+  } else {
+    rob_df <- perm_df[, "Symbol", drop = FALSE]
+  }
+  
+  rank_df <- allrank$Human[[x]][, keep_cols]
+  
+  rank_df <- dplyr::rename(rank_df, 
+                           Mean_encpipe = Mean_bind,
+                           logFC_encpipe = Bind_logFC,
+                           FDR_encpipe = Bind_adj_Pval)
+  
+  
+  all_df <- left_join(rank_df, rob_df, by = "Symbol") %>% 
+    left_join(perm_df, by = "Symbol")
+  
+})
+names(allrank_hg) <- tf_rank_hg
+
+
+
+allrank_mm <- lapply(tf_rank_mm, function(x) {
+  
+  perm_df <- bind_perm_mm$Fit[[x]][, c("logFC", "adj.P.Val")] %>% 
+    rownames_to_column(var = "Symbol") %>% 
+    left_join(bind_perm_mm$Group_mean[[x]], by = "Symbol")
+  colnames(perm_df) <- c("Symbol", "logFC_permissive", "FDR_permissive", "Mean_permissive")
+  
+  if (!is.null(bind_mm$Fit[[x]])) {
+    
+    rob_df <- bind_mm$Fit[[x]][, c("logFC", "adj.P.Val")] %>% 
+      rownames_to_column(var = "Symbol") %>% 
+      left_join(bind_mm$Group_mean[[x]], by = "Symbol")
+    colnames(rob_df) <- c("Symbol", "logFC_robust", "FDR_robust", "Mean_robust")
+    
+  } else {
+    rob_df <- perm_df[, "Symbol", drop = FALSE]
+  }
+  
+  rank_df <- allrank$Mouse[[str_to_title(x)]][, keep_cols]
+  
+  rank_df <- dplyr::rename(rank_df, 
+                           Mean_encpipe = Mean_bind,
+                           logFC_encpipe = Bind_logFC,
+                           FDR_encpipe = Bind_adj_Pval)
+  
+  
+  all_df <- left_join(rank_df, rob_df, by = "Symbol") %>% 
+    left_join(perm_df, by = "Symbol")
+  
+})
+names(allrank_mm) <- tf_rank_mm
+
+
+# Get cor and topn of encpipe vs robust/permissive
+
+
+compare_hg <- lapply(tf_rank_hg, function(x) {
+  
+  df <- allrank_hg[[x]]
+  
+  cor_perm <- cor(df$Mean_permissive, df$Mean_encpipe, use = "pairwise.complete.obs")
+  
+  topn_perm <- length(intersect(
+    arrange(df, desc(Mean_encpipe))$Symbol[1:topn],
+    arrange(df, desc(Mean_permissive))$Symbol[1:topn]
+  ))
+  
+  if (x %in% names(bind_hg$Group_mean)) {
+    
+    cor_rob <- cor(df$Mean_robust, df$Mean_encpipe, use = "pairwise.complete.obs")
+    
+    topn_rob <- length(intersect(
+      arrange(df, desc(Mean_encpipe))$Symbol[1:topn],
+      arrange(df, desc(Mean_robust))$Symbol[1:topn]
+    ))
+    
+  } else {
+    
+    cor_rob <- topn_rob <- NA
+  }
+  
+  data.frame(
+    Cor_robust = cor_rob,
+    Cor_permissive = cor_perm,
+    Topn_robust = topn_rob,
+    Topn_permissive = topn_perm
+  )
+  
+})
+names(compare_hg) <- tf_rank_hg
+
+compare_hg <- do.call(rbind, compare_hg)
+rownames(compare_hg) <- paste0(rownames(compare_hg), "_Human")
+
+
+
+compare_mm <- lapply(tf_rank_mm, function(x) {
+  
+  df <- allrank_mm[[x]]
+  
+  cor_perm <- cor(df$Mean_permissive, df$Mean_encpipe, use = "pairwise.complete.obs")
+  
+  topn_perm <- length(intersect(
+    arrange(df, desc(Mean_encpipe))$Symbol[1:topn],
+    arrange(df, desc(Mean_permissive))$Symbol[1:topn]
+  ))
+  
+  if (x %in% names(bind_mm$Group_mean)) {
+    
+    cor_rob <- cor(df$Mean_robust, df$Mean_encpipe, use = "pairwise.complete.obs")
+    
+    topn_rob <- length(intersect(
+      arrange(df, desc(Mean_encpipe))$Symbol[1:topn],
+      arrange(df, desc(Mean_robust))$Symbol[1:topn]
+    ))
+    
+  } else {
+    
+    cor_rob <- topn_rob <- NA
+  }
+  
+  data.frame(
+    Cor_robust = cor_rob,
+    Cor_permissive = cor_perm,
+    Topn_robust = topn_rob,
+    Topn_permissive = topn_perm
+  )
+  
+})
+names(compare_mm) <- tf_rank_mm
+
+compare_mm <- do.call(rbind, compare_mm)
+rownames(compare_mm) <- paste0(rownames(compare_mm), "_Mouse")
+
+
+# Heatmap/table of ENCODE vs unibind
+
+
+mat_cor <- round(rbind(compare_hg, compare_mm)[, 1:2], 3)
+mat_cor <- mat_cor[sort(rownames(mat_cor)),]
+colnames(mat_cor) <- c("Robust", "Permissive")
+
+mat_topn <- round(rbind(compare_hg, compare_mm)[, 3:4], 3)
+mat_topn <- mat_topn[sort(rownames(mat_topn)),]
+colnames(mat_topn) <- c("Robust", "Permissive")
+
+
+pal_length <- 100
+bluered_pal <- colorRampPalette(c("#0571b0", "white", "#ca0020"))(pal_length)
+color_breaks <- seq(0, 1, length.out = pal_length)
+green_pal <- c('#f7fcf5','#e5f5e0','#c7e9c0','#a1d99b','#74c476','#41ab5d','#238b45')
+
+
+
+pheatmap(mat_cor,
+         cluster_cols = FALSE,
+         cluster_rows = FALSE,
+         color = bluered_pal,
+         breaks = color_breaks,
+         display_numbers = TRUE,
+         number_format = "%.2f",
+         number_color = "black",
+         fontsize = 30,
+         cellwidth = 50,
+         cellheight = 50,
+         angle_col = 90,
+         gaps_col = 1)
+
+
+pheatmap(mat_topn,
+         cluster_cols = FALSE,
+         cluster_rows = FALSE,
+         color = green_pal,
+         display_numbers = TRUE,
+         number_format = "%.0f",
+         number_color = "black",
+         fontsize = 30,
+         cellwidth = 50,
+         cellheight = 50,
+         angle_col = 90,
+         gaps_col = 1)
