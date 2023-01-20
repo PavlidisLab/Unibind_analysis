b97cb6e4a3a633d0f9ac676768e5e107ed3121c2
Alexander-Morin <alexm.14@outlook.com>
2023-01-19 16:09:23 -0800

    rename and config export of Grlist

---
+++ R/06_save_unibind_as_granges.R
@@ -0,0 +1,69 @@
+## Saving Unibind experiments as GenomicRanges list object
+## -----------------------------------------------------------------------------
+
+library(tidyverse)
+library(GenomicRanges)
+source("R/Utils/functions.R")
+source("R/00_config.R")
+
+# ENCODE blacklisted regions to be filtered
+bl_hg <- bl_to_gr(read.delim(bl_path_hg, stringsAsFactors = FALSE))
+bl_mm <- bl_to_gr(read.delim(bl_path_mm, stringsAsFactors = FALSE))
+
+# Meta data of experiments to be loaded
+meta_l <- readRDS(meta_outfile)
+
+
+# Load each experiment of the input/meta df into a list of GR objects
+# ------------------------------------------------------------------------------
+
+
+load_gr <- function(dir, input_df, bl_gr) {
+  
+  gr_l <- lapply(1:nrow(input_df), function(x) {
+    
+    path <- file.path(dir, input_df$Symbol[x], input_df$File[x])
+    
+    tryCatch({
+      
+      peak_gr <- load_unibind(path) %>% 
+        unibind_to_gr(bl_gr)
+      
+      message(input_df$File[x], " complete ", Sys.time())
+      
+      return(peak_gr)
+      
+    }, error = function(e) NULL)
+    
+  })
+  
+  names(gr_l) <- input_df$File
+  return(gr_l)
+}
+
+
+# Load human permissive and create subset list of robust collection
+
+gr_perm_hg <- load_gr(dir = perm_path_hg, 
+                      input_df = meta_l$Permissive_hg, 
+                      bl_gr = bl_hg)
+
+gr_rob_hg <- gr_perm_hg[intersect(meta_l$Robust_hg$File, meta_l$Permissive_hg$File)]
+
+# Ditto for mouse
+
+gr_perm_mm <- load_gr(dir = perm_path_mm, 
+                      input_df = meta_l$Permissive_mm, 
+                      bl_gr = bl_mm)
+
+gr_rob_mm <- gr_perm_mm[intersect(meta_l$Robust_mm$File, meta_l$Permissive_mm$File)]
+
+
+# Save out
+# ------------------------------------------------------------------------------
+
+
+saveRDS(gr_perm_hg, file = gr_path_perm_hg)
+saveRDS(gr_rob_hg, file = gr_path_rob_hg)
+saveRDS(gr_perm_mm, file = gr_path_perm_mm)
+saveRDS(gr_rob_mm, file = gr_path_rob_mm)
