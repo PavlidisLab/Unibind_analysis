eb6001147b304db425ed51b065e302294c9b1b1d
Alexander-Morin <alexm.14@outlook.com>
2025-03-13 17:44:06 -0700

    doc; add functions for downloading refseq and blacklists

--- R/02_download_data.R
+++ R/02_download_data.R
@@ -1,8 +1,4 @@
-## Download Unibind data, ...
-## TODO: port over pcoding/ccre/etc table calls from TR agg OR make package
-## TODO: robust/permissive def
-## TODO: look at compressed https://unibind.uio.no/static/data/20220914/bulk_Robust/Homo_sapiens/hg38_compressed_TFBSs.bed.gz
-## TODO: download DHS
+## Download Unibind data
 ## -----------------------------------------------------------------------------
 
 library(tidyverse)
@@ -12,9 +8,12 @@ options(timeout = 1000)  # default 60 was insufficient
 
 
 # Unibind data: Robust versus Permissive collection of peaks are each saved
-# in a single directory for all experiments.
-# Permissive:
-# Robust: 
+# in a single directory for all experiments. Permissive collection is all 
+# experiments; robust are experiments where the canonical motif is enriched
+# near the summits of the given experiment's peaks. Note that experiments
+# present in both collections are identical -- robust does not filter away peaks,
+# just datasets.
+# https://unibind.uio.no/docs/
 # ------------------------------------------------------------------------------
 
 
@@ -54,10 +53,110 @@ for (i in 1:nrow(input_df)) {
 
 
 
-# UK Biobank depletion ranks
+# Refseq select protein coding genes
+# Using ensembl range formatting: no 'chr' prefix and strand as 1/-1
+# https://www.ncbi.nlm.nih.gov/refseq/refseq_select/
+# Table schema (mouse) http://genome.ucsc.edu/cgi-bin/hgTables?hgsid=1827432270_fzD31oCmH8TcANzQ82Yzz7h0RKlz&hgta_doSchemaDb=mm10&hgta_doSchemaTable=ncbiRefSeqSelect
+# --- Raw columns
+# bin	
+# V2 == name == Refseq_ID	
+# V3 == chrom == Chromsome	
+# V4 == strand == Strand	
+# V5 == txStart == Start	
+# V6 == txEnd == End	
+# cdsStart	
+# cdsEnd	
+# exonCount	
+# exonStarts	
+# exonEnds	
+# score	
+# V13 == name2 == Symbol	
+# cdsStartStat	
+# cdsEndStat	
+# exonFrames
+# ------------------------------------------------------------------------------
 
-dr_url <- "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC9329122/bin/41586_2022_4965_MOESM3_ESM.gz"
 
-if (!file.exists(dr_path)) {
-  download.file(dr_url, dr_path)  
+download_refseq <- function(outfile, 
+                            species) {  # Human|Mouse
+  
+  if (file.exists(outfile)) return(message(outfile, " already exists!"))
+  
+  if (species == "Mouse") {
+    link <- "http://hgdownload.soe.ucsc.edu/goldenPath/mm10/database/ncbiRefSeqSelect.txt.gz"
+    chr <- c(1:19, "MT", "X", "Y")
+  } else if (species == "Human") {
+    link <- "http://hgdownload.soe.ucsc.edu/goldenPath/hg38/database/ncbiRefSeqSelect.txt.gz"
+    chr <- c(1:22, "MT", "X", "Y")
+  }
+  
+  download.file(link, outfile)
+  
+  refseq <- read.delim(outfile, stringsAsFactors = FALSE, header = FALSE)
+  
+  refseq <- dplyr::select(refseq, c(V3, V5, V6, V4, V2, V13))
+  
+  colnames(refseq) <- c("Chromosome",
+                        "Start",
+                        "End",
+                        "Strand",
+                        "Refseq_ID",
+                        "Symbol")
+  
+  refseq <- refseq %>% 
+    mutate(
+      Strand = ifelse(Strand == "+", 1, -1),
+      Transcription_start_site = ifelse(Strand == 1, Start, End),
+      Chromosome = str_replace(Chromosome, "chr", "")) %>% 
+    filter(Chromosome %in% chr) %>% 
+    arrange(match(Chromosome, chr), Transcription_start_site) %>% 
+    dplyr::relocate(Transcription_start_site, .after = Chromosome)
+  
+  
+  write.table(refseq,
+              quote = FALSE,
+              row.names = FALSE,
+              sep = "\t",
+              file = outfile)
+  
 }
+
+
+
+download_refseq(outfile = ref_path_hg, species = "Human")
+download_refseq(outfile = ref_path_mm, species = "Mouse")
+
+
+# ENCODE blacklisted regions
+# ------------------------------------------------------------------------------
+
+
+chr_hg <- c(1:22, "MT", "X", "Y")
+chr_mm <- c(1:19, "MT", "X", "Y")
+
+bl_url_hg <- "https://www.encodeproject.org/files/ENCFF356LFX/@@download/ENCFF356LFX.bed.gz"
+bl_url_mm <- "https://www.encodeproject.org/files/ENCFF547MET/@@download/ENCFF547MET.bed.gz"
+
+
+download_blacklist <- function(outfile, url, chr) {
+  
+  if (!file.exists(outfile)) {
+    
+    download.file(url, destfile = outfile)
+    bl <- read.delim(outfile, header = FALSE)
+    colnames(bl) <- c("Chromosome", "Start", "End")
+    bl$Chromosome <- gsub("chr", "", bl$Chromosome)
+    bl <- bl[order(match(bl$Chromosome, chr)),]
+    
+    write.table(bl,
+                sep = "\t",
+                quote = FALSE,
+                row.names = FALSE,
+                file = outfile)
+  }
+  
+}
+
+
+download_blacklist(bl_path_hg, bl_url_hg, chr_hg)
+download_blacklist(bl_path_mm, bl_url_mm, chr_mm)
